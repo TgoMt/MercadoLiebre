d2962c7ae25909fbed2e4a4b73df0329cd1ec685
TgoMt <thiaguiduran@hotmail.com>
2022-04-14 00:02:51 -0300

    Agregando mas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require ('path');
 
 app.use(express.static('public'));
 
-app.listen(3003, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log('Servidor corre en http://localhost:3003')
 });
 
